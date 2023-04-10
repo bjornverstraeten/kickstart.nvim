a10076e7c58c36e48439e9e4ad61e739db9c2aaf
bjornverstraeten <verstraeten.bjorn@hotmail.com>
2023-04-10 14:04:46 +0200

    fix snippet libraries

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -7,16 +7,16 @@ vim.keymap.set({ 'n', 'v' }, '<leader>h', ':noh<CR>', {})
 
 -- Using system clipboard
 vim.g.clipboard = {
-  name = 'GPaste',
-  copy = {
-    ['+'] = {'gpaste-client', 'add'},
-    ['*'] = {'gpaste-client', 'add'},
-  },
-  paste = {
-    ['+'] = {'gpaste-client', '--use-index', 'get', '0'},
-    ['*'] = {'gpaste-client', '--use-index', 'get', '0'},
-  },
-  cache_enabled = true,
+    name = 'GPaste',
+    copy = {
+        ['+'] = { 'gpaste-client', 'add' },
+        ['*'] = { 'gpaste-client', 'add' },
+    },
+    paste = {
+        ['+'] = { 'gpaste-client', '--use-index', 'get', '0' },
+        ['*'] = { 'gpaste-client', '--use-index', 'get', '0' },
+    },
+    cache_enabled = true,
 }
 vim.opt.clipboard = 'unnamedplus'
 
@@ -35,12 +35,12 @@ vim.cmd([[hi Normal guibg=none]])
 -- Set lualine as statusline
 -- See `:help lualine.txt`
 require('lualine').setup {
-  options = {
-    icons_enabled = false,
-    theme = 'gruvbox',
-    component_separators = '|',
-    section_separators = '',
-  },
+    options = {
+        icons_enabled = false,
+        theme = 'gruvbox',
+        component_separators = '|',
+        section_separators = '',
+    },
 }
 
 -- [[ Setting providers ]]
@@ -50,6 +50,6 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
 -- -- [[ Configure LuaSnip ]]
-require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"})
--- require("luasnip.loaders.from_snipmate").lazy_load({paths = "~/.local/share/nvim/site/pack/packer/start/vim-snippets/snippets"})
-
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnip/" })
+require("luasnip.loaders.from_snipmate").lazy_load()
+-- { paths = "~/.local/share/nvim/lazy/vim-snippets" }
