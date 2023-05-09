c6bdf37e9a5e00e16c4ee123bf9fae0101bde29c
bjornverstraeten <verstraeten.bjorn@hotmail.com>
2023-05-09 15:57:49 +0200

    fix gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,15 +2,19 @@ return {
     'tpope/vim-fugitive',
     'tpope/vim-rhubarb',
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
+    {
+        'lewis6991/gitsigns.nvim',
         -- See `:help gitsigns.txt`
-        signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-        },
-    },
+        config = function()
+            require('gitsigns').setup({
+                signs = {
+                    add = { text = '+' },
+                    change = { text = '~' },
+                    delete = { text = '_' },
+                    topdelete = { text = '‾' },
+                    changedelete = { text = '~' },
+                },
+            })
+        end
+    }
 }
