43f70422e49416515f2c3f5f196ff4a9d8f98352
bjornverstraeten <verstraeten.bjorn@hotmail.com>
2023-05-17 17:13:39 +0200

    follow the primeagen config

---
+++ lua/neuts/plugins.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+	},
+	{	
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ":TSUpdate",
+	},
+	{
+		'nvim-treesitter/playground'
+	},
+	{
+		'ThePrimeagen/harpoon'
+	},
+	{
+		'mbbill/undotree'
+	},
+	{
+		'tpope/vim-fugitive'
+	},
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		dependencies = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+				'williamboman/mason.nvim',
+				build = function()
+					pcall(vim.cmd, 'MasonUpdate')
+				end,
+			},
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	},
+}
