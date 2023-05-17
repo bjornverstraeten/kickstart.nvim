85e5d33dbb46a4d8d293db57b30679cc77b0b942
bjornverstraeten <verstraeten.bjorn@hotmail.com>
2023-05-17 19:09:26 +0200

    tweaks

--- lua/neuts/plugins.lua
+++ lua/neuts/plugins.lua
@@ -8,7 +8,7 @@ return {
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 	},
-	{	
+	{
 		'nvim-treesitter/nvim-treesitter',
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter-textobjects',
