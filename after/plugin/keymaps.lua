433999751d2e512c88a839b892e010ce61bab657
bjoverst <bjorn.verstraeten@ugent.be>
2025-04-12 14:18:51 +0200

    toggle terminal

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -6,9 +6,6 @@ vim.keymap.set('n', '<leader>X', '<CMD>source %<CR>')
 vim.keymap.set('n', '<leader>x', ':.lua<CR>')
 vim.keymap.set('v', '<leader>x', ':lua<CR>')
 
--- Redraw ctags
--- vim.keymap.set('n', '<leader>t', ':!ctags -R . <CR>:redraw!<CR>', { noremap = true, silent = true, desc = 'redraw [T]ags' })
-
 --  Remap escape
 vim.keymap.set('i', 'jj', '<ESC>')
 vim.keymap.set('t', 'jj', [[<C-\><C-n>]])
@@ -24,21 +21,23 @@ vim.keymap.set('n', '<leader>Y', [["+Y]])
 vim.keymap.set('n', '<c-u>', '<c-u>zz')
 vim.keymap.set('n', '<c-d>', '<c-d>zz')
 
-local term_winid = nil
-
+local term_bufid, term_winid
 vim.keymap.set('n', '<leader>tt', function()
-  if term_winid and vim.api.nvim_win_is_valid(term_winid) then
-    -- If terminal is visible, close the window
-    vim.api.nvim_win_close(term_winid, true)
-    term_winid = nil
-  else
-    -- Otherwise, open a new terminal window
-    vim.cmd('belowright new')  -- open horizontal split
-    vim.cmd('term')
+  if not term_bufid or not vim.api.nvim_buf_is_valid(term_bufid) then
+    vim.cmd 'belowright new'
+    vim.cmd 'term'
+    term_bufid = vim.api.nvim_get_current_buf()
     term_winid = vim.api.nvim_get_current_win()
     vim.api.nvim_win_set_height(term_winid, 15)
+  elseif term_winid and vim.api.nvim_win_is_valid(term_winid) then
+      vim.api.nvim_win_close(term_winid, true)
+  else
+    vim.cmd 'split'
+    term_winid = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_height(term_winid, 15)
+    vim.api.nvim_win_set_buf(term_winid, term_bufid)
   end
-end, { desc = "[T]oggle [T]erminal" })
+end, { desc = '[T]oggle [T]erminal' })
 
 -- Fuzzy search
 local builtin = require 'telescope.builtin'
@@ -69,7 +68,8 @@ vim.keymap.set('n', '<leader>sh', themed_picker(builtin.help_tags), { desc = '[S
 vim.keymap.set('n', '<leader>sf', project_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sr', themed_picker(builtin.resume), { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>s.', themed_picker(builtin.oldfiles), { desc = '[S]earch Recent Files ("." for repeat)' })
-vim.keymap.set('n', '<leader><leader>', themed_picker(builtin.buffers), { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>sn', themed_picker(builtin.find_files, { cwd = vim.fn.stdpath 'config' }), { desc = '[S]earch [N]eovim files' })
+vim.keymap.set('n', '<leader>sb', themed_picker(builtin.buffers), { desc = '[ ] Find existing buffers' })
+vim.keymap.del('n', '<leader><leader>')
 
 -- vim: ts=2 sts=2 sw=2 et
